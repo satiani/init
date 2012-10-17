b6a595ccc1a7ff2b415b817dfc71db7121574e35
Samer Atiani <satiani@gmail.com>
2012-10-17 01:49:05 -0400

    Remove unused file

--- bin/xcopy.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-# Utility to copy any X text selection into clipboard. Bind it to some key in
-# metacity for happinness and world peace, especially when using xterm or urxvt
-# like terminals.
-xclip -o sel pri | xclip -sel clip
