1aaf5bb8a8f9e08683f94cd7722bf1ffeb6f8cb7
Samer Atiani <satiani@gmail.com>
2012-10-25 13:29:02 -0400

    Make deploys more automatic

--- .vim/plugin/etsy.vim
+++ .vim/plugin/etsy.vim
@@ -1,3 +1,4 @@
 au FileType php inoremap ^sp Logger::log_info("spock: ");<Esc>hhi
 au FileType php inoremap ^ap // FIXME: APC Warning: release this in a second push
 au FileType php inoremap ^in require_once 'bootstrap/web.php';
+au FileType php inoremap ^pr print "ECHO:  \n";<Esc>hhhhi
--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,10 @@ SCRIPT_DIR=$(cd `dirname $0` && pwd)
 mkdir $HOME/vimswap
 
 for i in `find $SCRIPT_DIR -maxdepth 1 -name '.*' | grep -v git`; do
+    home_path=$HOME/`basename $i`
+    if [ -f $home_path -o -L $home_path ]; then
+        mv $home_path ${home_path}.old;
+    fi;
     ln -sv $i $HOME
 done
 
