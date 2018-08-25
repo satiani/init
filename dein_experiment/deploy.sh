d50c2ca24ab1bdf6598b7f1762f6a2c7b70d1f79
Samer Atiani <satiani@gmail.com>
2018-08-25 00:06:51 +0000

    dein deploy.sh

--- dein_experiment/deploy.sh
+++ dein_experiment/deploy.sh
@@ -3,8 +3,16 @@
 SCRIPT_DIR=$(cd `dirname $0` && pwd)
 
 # install rust
-curl https://sh.rustup.rs -sSf | sh
+curl http://sh.rustup.rs -sSf | sh
 export PATH=~/.cargo/bin/:$PATH
+
+# install ripgrep using rust package manager
 cargo install ripgrep
+
+# install dein
+source $SCRIPT_DIR/lib.sh
+install_dein ~/.cache/dein
+
+# symbolic links
 ln -sv $SCRIPT_DIR/.vimrc $HOME
 ln -sv $SCRIPT_DIR/.vim $HOME
