e215b82c4d00e00d003c7175438aa8616a51fa7e
Samer Atiani <satiani@gmail.com>
2018-08-30 19:41:29 +0000

    Adopt new git push strategy defaults

--- dein_experiment/deploy.sh
+++ dein_experiment/deploy.sh
@@ -35,3 +35,4 @@ git config --global user.name "Samer Atiani"
 git config --global user.email "satiani@gmail.com"
 git config --global color.ui auto
 git config --global alias.st status
+git config --global push.default simple
