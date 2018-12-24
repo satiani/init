616db2fb63994990178b178891007afb3efda43c
Samer Atiani <satiani@gmail.com>
2018-12-24 15:27:00 +0000

    push.sh

--- git.sh
+++ git.sh
@@ -6,5 +6,7 @@ git config --global user.email "satiani@gmail.com"
 git config --global color.ui auto
 git config --global alias.st status
 git config --global push.default simple
+git config --global mergetool.nvimdiff.cmd 'nvim -f -c "Gdiff" "$MERGED"'
+git config --global merge.tool nvimdiff
 # }}}
 echo "Done"
