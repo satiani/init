cd5014915c124a28ea78910214544a48b4e92ce0
Samer Atiani <satiani@gmail.com>
2020-11-30 22:24:17 -0500

    push.sh

--- .zsh/theme.zsh
+++ .zsh/theme.zsh
@@ -1,11 +1,8 @@
 if [ "$(whoami)" = "root" ]; then CARETCOLOR="cyan"; else CARETCOLOR="white"; fi
 
-FILTER_STR="inet addr:"
-CUT=":"
+FILTER_STR="inet[^6]"
 
-[ $ON_A_MAC == "true" ] && FILTER_STR="inet[^6]" && CUT=" "
-
-IP_ADDR=$(/sbin/ifconfig | grep "$FILTER_STR" | grep -v '127.0.0.1' | cut -d "$CUT" -f2 | awk '{ print $1}' | head -n 1)
+IP_ADDR=$(/sbin/ifconfig | grep "$FILTER_STR" | grep -v '127.0.0.1' | awk '{print $2}' | head -n 1)
 HOSTNAME=$(hostname)
 
 HOSTNAME="$HOSTNAME%{$reset_color%}"
