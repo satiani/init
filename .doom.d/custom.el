d1ab8e73e2022683a71c9e963e17c97e5f83961b
Samer Atiani <samer.atiani@datadoghq.com>
2022-12-30 14:11:40 -0500

    push.sh

---
+++ .doom.d/custom.el
@@ -0,0 +1,22 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(electric-pair-mode t)
+ '(electric-quote-mode nil)
+ '(org-agenda-files
+   '("/Users/samer.atiani/Dropbox/orgmode/master.org" "/Users/samer.atiani/Dropbox/orgmode/1on1.org" "/Users/samer.atiani/Dropbox/orgmode/allhands.org" "/Users/samer.atiani/Dropbox/orgmode/biweekly.org" "/Users/samer.atiani/Dropbox/orgmode/daily.org" "/Users/samer.atiani/Dropbox/orgmode/interviews.org" "/Users/samer.atiani/Dropbox/orgmode/journal.org" "/Users/samer.atiani/Dropbox/orgmode/management.org" "/Users/samer.atiani/Dropbox/orgmode/meeting_notes.org" "/Users/samer.atiani/Dropbox/orgmode/new_work.org" "/Users/samer.atiani/Dropbox/orgmode/personal.org" "/Users/samer.atiani/Dropbox/orgmode/personnel.org" "/Users/samer.atiani/Dropbox/orgmode/projects.org" "/Users/samer.atiani/Dropbox/orgmode/recruiting.org" "/Users/samer.atiani/Dropbox/orgmode/scratch.org" "/Users/samer.atiani/Dropbox/orgmode/workbook.org"))
+ '(org-link-frame-setup
+   '((vm . vm-visit-folder-other-frame)
+     (vm-imap . vm-visit-imap-folder-other-frame)
+     (gnus . org-gnus-no-new-news)
+     (file . find-file)
+     (wl . wl-other-frame)))
+ '(treemacs-follow-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
